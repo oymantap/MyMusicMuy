9c4ceab3a62a09f1d3cb71001bc94b5c36213976
oymantap <fcmasiasoasikgaguna@gmail.com>
2026-05-07 13:56:00 +0700

    Add files via upload

--- app/src/main/java/com/mymusic/muy/MainActivity.kt
+++ app/src/main/java/com/mymusic/muy/MainActivity.kt
@@ -1,1 +1,21 @@
+package com.mymusic.muy
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnPickFolder = findViewById<Button>(R.id.btnPickFolder)
+
+        btnPickFolder.setOnClickListener {
+            // Ini nanti tempat kita panggil Folder Picker
+            Toast.makeText(this, "Membuka Penyimpanan...", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
